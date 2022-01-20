c8f1869d833f18b6832aa652865bad0ad1e8fd38
Natchaya <007347@g-able.com>
2022-01-20 13:50:58 +0700

    first commit

---
+++ src/main/java/com/example/crudgablemintbombchuan/CrudGableMintBombChuanApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudgablemintbombchuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudGableMintBombChuanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudGableMintBombChuanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crudgablemintbombchuan/CrudGableMintBombChuanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudgablemintbombchuan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudGableMintBombChuanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
